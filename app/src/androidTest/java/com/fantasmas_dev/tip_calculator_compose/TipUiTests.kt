192e1ec5aa1744f43670b9fa276a8c77a8e0e5ca
FantasmasDe <fantasmas.dev@gmail.com>
2024-08-01 00:12:23 +0300

    1. стиль

--- app/src/androidTest/java/com/fantasmas_dev/tip_calculator_compose/TipUiTests.kt
+++ app/src/androidTest/java/com/fantasmas_dev/tip_calculator_compose/TipUiTests.kt
@@ -1,11 +1,5 @@
 package com.fantasmas_dev.tip_calculator_compose
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
@@ -23,16 +17,16 @@ class TipUiTests {
     fun calculate_20_percent_tip() {
         composeTestRule.setContent {
             Tip_calculator_composeTheme {
-                        TipTimeLayout()
-                }
+                TipTimeLayout()
             }
-            composeTestRule.onNodeWithText("Сумма чека")
-                .performTextInput("10")
-            composeTestRule.onNodeWithText("Процент чаевых")
-                .performTextInput("20")
-            val expectedTip = NumberFormat.getCurrencyInstance().format(2)
-            composeTestRule.onNodeWithText("Чаевые: $expectedTip").assertExists(
-                "Нода с данным текстом не найдена"
-            )
         }
+        composeTestRule.onNodeWithText("Сумма чека")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Процент чаевых")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Чаевые: $expectedTip").assertExists(
+            "Нода с данным текстом не найдена"
+        )
     }
+}
